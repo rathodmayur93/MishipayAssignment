8b68a9cd439504d5e6ce9e4019fc2ff4462a1d88
Mayur Rathod <mayur.rathod@devjisystems.com>
2021-01-02 15:06:36 +0300

    first commit

---
+++ MishiPay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MishiPay
+//
+//  Created by ds-mayur on 02/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
